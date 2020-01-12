6ec088f0950bfd5788023bacb408befdf2c972b6
sundar93 <sundar9393@gmail.com>
2020-01-12 17:17:55 +0530

    Added Header Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './Header';
 
 function App() {
   return (
     <div>
-    Phone Directory  
+      <Header/>
+    <button>Add</button>
+      <div>
+        <span>Name</span><br />
+        <span>Phone</span>
+      </div>
     </div>
   );
 }
